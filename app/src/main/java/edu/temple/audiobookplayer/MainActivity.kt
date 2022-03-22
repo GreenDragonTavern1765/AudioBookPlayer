cb9dc9c1b205bd6943689f14ec846484e2ebdb72
Sam Sepial <zprestoz@protonmail.com>
2022-03-22 13:47:16 -0400

    Successfully implemented the RecyclerView, it uses a grid layout allowing for 2 books to be displayed at one time.

--- app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
@@ -2,16 +2,22 @@ package edu.temple.audiobookplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bookAdapter: BookAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.bookRecyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
+        recyclerView.layoutManager = GridLayoutManager(applicationContext, 2)
+        bookAdapter = BookAdapter(applicationContext)
+        recyclerView.adapter = bookAdapter
+        bookAdapter.setDataList()
     }
 }
